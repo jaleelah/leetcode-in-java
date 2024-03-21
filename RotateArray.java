ab116e959b6261fd73d7c529ebdc1238d56ae323
jaleelah <91485585+jaleelah@users.noreply.github.com>
2024-03-20 22:18:22 -0400

    Update RotateArray.java

--- RotateArray.java
+++ RotateArray.java
@@ -1,14 +1,12 @@
 public class RotateArray {
     public void rotate(int[] nums, int k) {
-        public void rotate(int[] nums, int k) {
-            k = k % nums.length;
-            int[] ref = new int[nums.length];
-            for(int i = 0; i < nums.length; i++)
-                ref[i] = nums[i];
-            for(int i = 0; i < k; i++)
-                nums[i] = ref[nums.length - (k - i)];
-            for(int i = k; i < nums.length; i++)
-                nums[i] = ref[i - k];
-        }
+        k = k % nums.length;
+        int[] ref = new int[nums.length];
+        for(int i = 0; i < nums.length; i++)
+            ref[i] = nums[i];
+        for(int i = 0; i < k; i++)
+            nums[i] = ref[nums.length - (k - i)];
+        for(int i = k; i < nums.length; i++)
+            nums[i] = ref[i - k];
     }
 }
