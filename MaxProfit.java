dde5e10520fe6ee511c33038c392c6dec20f520e
jaleelah <91485585+jaleelah@users.noreply.github.com>
2024-03-26 12:17:44 -0400

    Solved Best Time to Buy and Sell Stock

---
+++ MaxProfit.java
@@ -0,0 +1,11 @@
+class MaxProfit {
+	public int maxProfit(int[] prices) {
+	        int max = 0;
+	        int floor = prices[0];
+	        for(int i = 1; i < prices.length; i++) {
+	            if(prices[i] - floor > max) max = prices[i] - floor;
+	            if(prices[i] < floor) floor = prices[i];
+	        }
+	        return max;
+	    }
+}
